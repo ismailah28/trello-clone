8d876807c356824304a03d8e165fb13fef7f0ba9
Ismaila Hassan <ismailah28@gmail.com>
2020-03-14 06:07:01 +0100

    implement board markup and state

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="board"></div>
+  <div class="board">
+    <div class="flex flex-row items-start">
+      <div
+        class="column"
+        v-for="(column, $columnIndex) in board.columns"
+        :key="$columnIndex"
+      >
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div
+            class="task"
+            v-for="(task, $taskIndex) in column.tasks"
+            :key="$taskIndex"
+          >
+            <span class="w-full flex-shrink-0 font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              v-if="task.description"
+              class="w-full flex-shrink-0 mt-1 text-sm"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["board"])
+  }
+};
 </script>
 
 <style lang="css">
